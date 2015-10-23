ca261590fe244048834612d57318ac9c48f1609e
Umberto Raimondi <me@uraimo.com>
2015-10-23 19:16:46 +0200

    Remove useless stuff from the example

--- SwizzlingInSwift/main.swift
+++ SwizzlingInSwift/main.swift
@@ -26,31 +26,17 @@ extension TestSwizzling {
             static var token: dispatch_once_t = 0;
         }
         
-        // make sure this isn't a subclass
-        if (self !== TestSwizzling.self)
-        {
-            return;
-        }
         
         // Perform this one time only
         dispatch_once(&Static.token)
-            {
+        {
                 let originalSelector = Selector("methodOne");
                 let swizzledSelector = Selector("methodTwo");
                 
                 let originalMethod = class_getInstanceMethod(self, originalSelector);
                 let swizzledMethod = class_getInstanceMethod(self, swizzledSelector);
                 
-                let didAddMethod = class_addMethod(self, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
-                
-                if didAddMethod
-                {
-                    class_replaceMethod(self, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
-                }
-                else
-                {
-                    method_exchangeImplementations(originalMethod, swizzledMethod);
-                }
+                method_exchangeImplementations(originalMethod, swizzledMethod);
         }
     }
     
